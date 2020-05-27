399c685f90fabe0052734aa7a92b305688c750f6
nu4h_ <62522294+Nu4h-dev@users.noreply.github.com>
2020-05-27 15:58:32 +0200

    Add files via upload

---
+++ src/tuto/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace tuto;
+
+use pocketmine\block\block;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener
+{
+
+    public function onEnable()
+    {
+        $this->getLogger()->info("le plugin a été correctement lancer");
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+
+
+    public function onBreak(BlockBreakEvent $event) {
+        $block = $event->getBlock();
+        if($block->getID() === Block::BEETROOT_BLOCK) {
+            $event->setDrops([Item::get(377, 0, 1)]);
+        }
+    }
+
+    public function onDisable()
+{
+    $this->getLogger()->info("le plugin a été correctement desactiver");
+}
+}
