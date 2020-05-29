95b9ce276f3d73e6c9ab36318bc24dc1da0e6167
nu4h_ <62522294+Nu4h-dev@users.noreply.github.com>
2020-05-29 21:14:38 +0200

    Update Main.php

--- src/nu4h/Main.php
+++ src/nu4h/Main.php
@@ -1,32 +1,47 @@
 <?php
 
-namespace nu4h;
+namespace tuto;
 
 use pocketmine\block\Block;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\config;
 
 
 class Main extends PluginBase implements Listener
 {
 
-
+private $config;
     public function onEnable()
     {
         $this->getLogger()->info("le plugin a été correctement lancer");
         $this->getServer()->getPluginManager()->registerEvents($this,$this);
 
-       
+@mkdir($this->getDataFolder());
+
+        if (!file_exists($this->getDataFolder() . "config.yml")) {
+
+            $this->saveResource('config.yml');       
 
     }
+    $config = new Config($this->getDataFolder() . 'config.yml', Config::YAML, array(
+   "block" => "BEETROOT_BLOCK",
+   "loot" => "377"
+));
+}
 
 
     public function onBreak(BlockBreakEvent $event) {
         $block = $event->getBlock();
-        if($block->getID() === Block::BEETROOT_BLOCK && mt_rand(1, 60) <= 1) {
-            $event->setDrops([Item::get(377, 0, 1)]);
+            $config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
+
+               if($block->getID() == $config->get('block')) {
+
+                    $event->setDrops($config->get('loot'));
+
+                
         }
     }
 
