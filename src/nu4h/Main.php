cac61aa2c2cfddb4a8005df3cdf8cd4f9beca798
nu4h_ <62522294+Nu4h-dev@users.noreply.github.com>
2020-06-16 12:38:34 +0200

    Update Main.php

--- src/nu4h/Main.php
+++ src/nu4h/Main.php
@@ -25,7 +25,7 @@ class Main extends PluginBase
     public function onBreak(BlockBreakEvent $event)
     {
         $config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
-        if ($event->getBlock()->getId() == $config->get('block')) && $event->getBlock()->getDamage() == $config->get('data', l) {
+        if ($event->getBlock()->getId() == $config->get('block')) && $event->getBlock()->getDamage() == $config->get('data') {
             $event->setDrops([Item::get($config->get('loot'), 0, 1)]);
         }
     }
