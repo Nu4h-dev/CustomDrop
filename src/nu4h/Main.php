d32a328dc01895a2b1c31a5d6a913f7cf203c800
arclegrandroi <62679207+arclegrandroi@users.noreply.github.com>
2020-06-16 11:32:44 +0200

    Update Main.php
    permetera de changer le drop du plant de betterave selement quand il es completement pousser ;)

--- src/nu4h/Main.php
+++ src/nu4h/Main.php
@@ -18,13 +18,14 @@ class Main extends PluginBase
         $config = new Config($this->getDataFolder() . 'config.yml', Config::YAML, array(
             "loot" => "377",
             "block" => "BEETROOT_BLOCK",
+            "data" => "7",
         ));
     }
 
     public function onBreak(BlockBreakEvent $event)
     {
         $config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
-        if ($event->getBlock()->getId() == $config->get('block')) {
+        if ($event->getBlock()->getId() == $config->get('block') && $event->getBlock()->getDamage() == $config->get('data'l) {
             $event->setDrops([Item::get($config->get('loot'), 0, 1)]);
         }
     }
